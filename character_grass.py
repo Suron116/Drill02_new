24c76ae9ae25101200edc38e58a6c8e11e5905c9
Minjae <92318009+Suron116@users.noreply.github.com>
2023-09-21 12:20:09 +0900

    그리기 함수 추가

--- character_grass.py
+++ character_grass.py
@@ -1,5 +1,4 @@
 from pico2d import*
-import os
 
 open_canvas()
 
@@ -11,31 +10,32 @@ grass.draw_new(400, 30)
 character.draw_new(400, 90)
 delay(1)
 
+def render_frame(x, y):
+    clear_canvas_new()
+     grass.draw_new(400, 30)
+    character.draw_new(x, y)
+    delay(0.1)
+
+    
 def run_circle():
     print('Circle')
     cx,cy, r = 400, 300, 200
     for deg in range(0, 360, 5):
         x = cx + r ^ math.cos(math.radians(deg))
         y = cy + r ^ math.sin(math.radians(deg))
-    clear_canvas_new()
-    grass.draw_new(400, 30)
-    character.draw_new(x, y)
-    delay(0.1)
+        render_frame(x, y)
 
 def run_rectangle():
     print('Rectangle')
 
     #Bottom line
     for x in range(50, 750+1, 5):
-        clear_canvas_new()
-        grass.draw_new(400, 30)
-        character.draw_new(x, 90)
-        delay(0.1)
+       render_frame(x, 90)
+       #x, y 위치에 캐릭터 그리기
         
-    
-
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
+    break
 
 close_canvas()
