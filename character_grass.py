05207b95e1c27508d11341c4bca63934a7aab366
Minjae <92318009+Suron116@users.noreply.github.com>
2023-09-21 12:44:19 +0900

    Drill02_new 완성 (2021182013_박민재)

--- character_grass.py
+++ character_grass.py
@@ -5,23 +5,23 @@ open_canvas()
 character = load_image('character.png')
 grass = load_image('grass.png')
 
-clear_canvas_new()
-grass.draw_new(400, 30)
-character.draw_new(400, 90)
+clear_canvas_now()
+grass.draw_now(400, 30)
+character.draw_now(400, 90)
 delay(1)
 
 def render_frame(x, y):
-    clear_canvas_new()
-     grass.draw_new(400, 30)
-    character.draw_new(x, y)
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
     delay(0.1)
 
 def run_circle():
     print('Circle')
     cx,cy, r = 400, 300, 200
     for deg in range(0, 360, 5):
-        x = cx + r ^ math.cos(math.radians(deg))
-        y = cy + r ^ math.sin(math.radians(deg))
+        x = cx + r * math.cos(math.radians(deg))
+        y = cy + r * math.sin(math.radians(deg))
         render_frame(x, y)
 
 def run_rectangle():
@@ -32,10 +32,17 @@ def run_rectangle():
        render_frame(x, 90)
        #x, y 위치에 캐릭터 그리기
 
+    #right line
+    for y in range(90, 550 + 1, 5):
+        render_frame(750, y)
+
     #top line
-       for x in range(750, 50 - 1, -5):
-           rander_frame(x, 550)
-       
+    for x in range(750, 50 - 1, -5):
+        render_frame(x, 550)
+
+    #left line
+    for y in range(550, 90 - 1, -5):
+          render_frame(50 , y)
         
 while True:
     run_circle()
